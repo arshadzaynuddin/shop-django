d831b357e47baf340f528d8b12fd7e329029c429
arshadzaynuddin <arshadzaynuddin@gmail.com>
2022-02-26 14:05:17 +0530

    Final Commit

---
+++ products/models.py
@@ -0,0 +1,16 @@
+from email.mime import image
+from xml.parsers.expat import model
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image = models.CharField(max_length=2500)
+
+class Offer(models.Model):
+    code =models.CharField(max_length=16)
+    description =models.CharField(max_length=255)
+    discount = models.FloatField()
